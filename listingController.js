dfb469b8d284102b266019bac614ae059a424dfa
Eric Gordon <egordon@ufl.edu>
2016-01-14 13:56:16 -0500

    Added Delete Buttons, Details table.

--- listingController.js
+++ listingController.js
@@ -1,14 +1,46 @@
 angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
   function($scope, Listings) {
     $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
+    $scope.newListing = {
+            "code": "____", 
+            "name": "____", 
+            "coordinates": {
+                "latitude": 0.000000, 
+                "longitude": 0.000000
+            }, 
+            "address": "Street, City, State, Zip"
+        };
+    $scope.detailedInfo = {
+            "code": "____", 
+            "name": "____", 
+            "coordinates": {
+                "latitude": 0.000000, 
+                "longitude": 0.000000
+            }, 
+            "address": "Street, City, State, Zip"
+        };
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push($scope.newListing);
+      $scope.newListing = {
+            "code": "____", 
+            "name": "____", 
+            "coordinates": {
+                "latitude": 0.000000, 
+                "longitude": 0.000000
+            }, 
+            "address": "Street, City, State, Zip"
+        };
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index,1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
